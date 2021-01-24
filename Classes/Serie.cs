f7b498e3e2da2fd1567e062a09753692486876ed
Moisés Mariano <moisesduartem@gmail.com>
2021-01-24 16:47:45 -0300

    feat: creating constructor

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -8,5 +8,13 @@ namespace DIO.Series.Classes
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Ano = ano;
+        }
     }
 }
