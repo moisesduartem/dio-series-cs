8908d0a683be5a9bbeeb983696d156d19534ada7
Moisés Mariano <moisesduartem@gmail.com>
2021-01-24 16:40:44 -0300

    feat: adding Serie attributes

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -2,6 +2,9 @@ namespace DIO.Series.Classes
 {
     public class Serie : EntidadeBase
     {
-
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
     }
 }
